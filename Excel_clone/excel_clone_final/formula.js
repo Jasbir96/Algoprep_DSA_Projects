fbb949eb45e60b4a031c9f46c250f25272f208a3
Jasbir <jasbir1.singh@cars24.com>
2024-05-31 20:05:55 +0530

    added final excel clone project

---
+++ Excel_clone/excel_clone_final/formula.js
@@ -0,0 +1,107 @@
+// cell -> formula remove /value set 
+for (let i = 0; i < AllGridCells.length; i++) {
+    AllGridCells[i].addEventListener("blur", function cellHelper(e) {
+        let content = AllGridCells[i].textContent;
+        let address = addressInput.value;
+        let { rid, cid } = getRidCidFromAddress(address);
+        let cellObject = db[rid][cid];
+        // set self ui as well as children new values and ui
+        if (cellObject.value == content) {
+            return;
+        }
+        if (cellObject.formula) {
+            removeFormula(address, cellObject.formula);
+            cellObject.formula = "";
+        }
+        setUI(content, rid, cid);
+    })
+}
+// formula bar
+// set Formula/update formula
+formulaInput.addEventListener("keydown", function (e) {
+    if (e.key == "Enter" && formulaInput.value != "") {
+        // fomrula get
+        let cFormula = formulaInput.value;
+        // address -> jispe formula jo value get wo set ho jaayegi 
+        let addressOfTheCell = addressInput.value;
+        let { rid, cid } = getRidCidFromAddress(addressOfTheCell);
+        let cellObject = db[rid][cid];
+        if (cellObject.formula != cFormula) {
+            removeFormula(addressOfTheCell, cellObject.formula);
+        }
+       
+        let value = evaluateFormula(cFormula);
+        setUI(value, rid, cid);
+        cellObject.formula = cFormula;
+        setFormula(addressOfTheCell, cFormula);
+    }
+})
+function evaluateFormula(formula) {
+    // ( A1 + A2 ) -> ( 10 + 20 )
+    let formulaEntities = formula.split(" ");
+    // [(,A1,+,A2,)]
+    for (let i = 0; i < formulaEntities.length; i++) {
+        let ascii = formulaEntities[i].charCodeAt(0);
+        if (ascii >= 65 && ascii <= 90) {
+            // address -> rid cId
+            let cellrcObj = getRidCidFromAddress(formulaEntities[i]);
+            // db -> value
+            let value = db[cellrcObj.rid][cellrcObj.cid].value;
+            // replace in formula
+            formula = formula.replace(formulaEntities[i], value);
+        }
+    }
+    // eval -> evaluate-> inbuilt 
+    let result = eval(formula);
+    return result;
+}
+function setUI(value, rid, cid) {
+    let tobeChangedCell = document.querySelector(`.grid .cell[rId='${rid}'][cId='${cid}']`);
+    tobeChangedCell.textContent = value;
+    db[rid][cid].value = value;
+    // change your children -> re-evaulate -> set ui
+    let childrenArr = db[rid][cid].children;
+    // B1
+    for (let i = 0; i < childrenArr.length; i++) {
+        let chriciobj = getRidCidFromAddress(childrenArr[i]);
+        let chCellObj = db[chriciobj.rid][chriciobj.cid];
+        let value = evaluateFormula(chCellObj.formula);
+        setUI(value, chriciobj.rid, chriciobj.cid)
+    }
+}
+//  to set a cell as children of a cell jispe depenedent 
+function setFormula(address, formula) {
+    // ( A1 + A2 ) -> ( 10 + 20 )
+    let formulaEntities = formula.split(" ");
+    // [(,A1,+,A2,)]
+    for (let i = 0; i < formulaEntities.length; i++) {
+        let ascii = formulaEntities[i].charCodeAt(0);
+        if (ascii >= 65 && ascii <= 90) {
+            // address -> rid cId
+            let parentrcObj = getRidCidFromAddress(formulaEntities[i]);
+            // db -> value
+            let children = db[parentrcObj.rid][parentrcObj.cid].children;
+            children.push(address);
+            // replace in formula
+
+        }
+    }
+}
+//  to set a cell as children of a cell jispe depenedent 
+function removeFormula(address, formula) {
+    // ( A1 + A2 ) -> ( 10 + 20 )
+    let formulaEntities = formula.split(" ");
+    // [(,A1,+,A2,)]
+    for (let i = 0; i < formulaEntities.length; i++) {
+        let ascii = formulaEntities[i].charCodeAt(0);
+        if (ascii >= 65 && ascii <= 90) {
+            // address -> rid cId
+            let parentrcObj = getRidCidFromAddress(formulaEntities[i]);
+            // db -> value
+            let children = db[parentrcObj.rid][parentrcObj.cid].children;
+            let idx = children.indexOf(address);
+            children.splice(idx, 1);
+            // replace in formula
+        }
+    }
+}
---
+++ Excel_clone/excel_clone_final/init.js
@@ -0,0 +1,232 @@
+
+// top row cells creation
+let topRow = document.querySelector(".top_row");
+// left col cells creation
+let leftCol = document.querySelector(".left_col");
+// grid
+let grid = document.querySelector(".grid");
+let addressInput = document.querySelector(".address_input");
+let formulaInput = document.querySelector(".formula_input");
+// *************************Menu Elements *****************************
+let fontSizeInput = document.querySelector(".font_size_input");
+let fontFamilyInput = document.querySelector(".font_family_input");
+let boldIcon = document.querySelector(".fa-bold");
+let underlineIcon = document.querySelector(".fa-underline");
+let italicIcon = document.querySelector(".fa-italic");
+let alignmentContainer = document.querySelector(".alignment_container");
+let textColorHInput = document.querySelector(".text_color");
+let textColorInput = document.querySelector(".fa-tint");
+let backgroundHInput = document.querySelector(".background_color");
+let backgroundInput = document.querySelector(".fa-fill-drip");
+let createSheetIcon = document.querySelector(".fa-plus");
+let sheetList = document.querySelector(".sheets-list");
+let firstSheet = document.querySelector(".sheet");
+for (let i = 0; i < 26; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "cell");
+    div.textContent = String.fromCharCode(65 + i);
+    topRow.appendChild(div)
+}
+
+for (let i = 1; i <= 100; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "cell");
+    div.textContent = i;
+    leftCol.appendChild(div)
+}
+// 2 d loop -> columns*rows
+
+for (let i = 0; i < 100; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 26; j++) {
+        let div = document.createElement("div");
+        div.setAttribute("class", "cell");
+       
+        div.setAttribute("contentEditable", "true")
+        //   every cell identification required 
+        div.setAttribute("rId", i);
+        div.setAttribute("cId", j);
+        row.appendChild(div);
+    }
+    grid.appendChild(row)
+}
+// default value put for every cell
+let sheetsDb = [];
+function initDB() {
+    let db = [];
+    for (let i = 0; i < 100; i++) {
+        let rowArr = [];
+        for (let j = 0; j < 26; j++) {
+            let cellObject = {
+                color: "black",
+                backgroundColor: "white",
+                fontFamily: "'Courier New'",
+                fontSize: 14,
+                halign: "center",
+                italic: false,
+                underline: false,
+                bold: false,
+                value: "",
+                formula: "",
+                children: []
+            }
+            rowArr.push(cellObject)
+        }
+        db.push(rowArr);
+    }
+    sheetsDb.push(db);
+}
+initDB();
+let db = sheetsDb[0];
+// if i click on any of the cells
+let AllGridCells = document.querySelectorAll(".grid .cell");
+for (let i = 0; i < AllGridCells.length; i++) {
+    AllGridCells[i].addEventListener("click", function (e) {
+        // // previous cell address
+        let prevAddress = addressInput.value;
+        if (prevAddress != "") {
+            let ridcidObj = getRidCidFromAddress(prevAddress);
+            // prev -> remove -> border
+            let prevCell = document
+                .querySelector
+                (`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
+            prevCell.style.border = "0.1px solid gray";
+            prevCell.style.borderRight = "none";
+            prevCell.style.borderTop = "none";
+        }
+        // 
+        // -> then i will get the address of that particular cell
+        let rid = AllGridCells[i].getAttribute("rId");
+        let cid = AllGridCells[i].getAttribute("cId");
+        //    get -> always in string 
+        rid = Number(rid);
+        cid = Number(cid);
+        addressInput.value = String.fromCharCode(cid + 65) + (rid + 1);
+        // cell styling bhi change
+        let cCell = AllGridCells[i];
+        cCell.style.border = "2px solid #1B9CFC";
+        // *****************2 way binding menu styling*****************
+        let cellObject = db[rid][cid];
+        // font size 
+        let fontSize = cellObject.fontSize;
+        fontSizeInput.value = fontSize;
+        boldIcon.classList.remove("selected");
+        italicIcon.classList.remove("selected");
+        underlineIcon.classList.remove("selected");
+        let optionElements = alignmentContainer.children;
+        for (let i = 0; i < optionElements.length; i++) {
+            optionElements[i].classList.remove("selected");
+        }
+        // boldness
+        if (cellObject.bold) {
+            boldIcon.classList.add("selected");
+        }
+        if (cellObject.italic) {
+            italicIcon.classList.add("selected");
+        }
+        if (cellObject.underline) {
+            underlineIcon.classList.add("selected");
+        }
+        if (cellObject.halign) {
+            for (let i = 0; i < optionElements.length; i++) {
+                let elementClasses = optionElements[i].classList;
+                let hAlignment = elementClasses[elementClasses.length - 1];
+                if (hAlignment == cellObject.halign) {
+                    elementClasses.add("selected");
+                }
+            }
+        }
+        formulaInput.value = cellObject.formula
+
+    })
+
+}
+// get first elem
+let firstCell = document.querySelector(".grid .cell[rId='0'][cId='0']");
+firstCell.click();
+firstCell.focus();
+function getRidCidFromAddress(address) {
+    // A-Z, 1-100
+    // B
+    let AsciiValue = address.charCodeAt(0);
+    let cid = AsciiValue - 65;
+    let rid = Number(address.substring(1)) - 1;
+    return {
+        rid: rid, cid: cid
+    }
+
+}
+firstSheet.addEventListener("click", function (e) {
+    //    list of sheet me se sabme se aap remove active sheet
+    for (let i = 0; i < sheetList.children.length; i++) {
+        sheetList.children[i].classList.remove("active-sheet")
+    }
+    // given sheet add kar lo 
+    firstSheet.classList.add("active-sheet");
+    db = sheetsDb[0];
+    setinitUI();
+
+})
+createSheetIcon.addEventListener("click", sheetHandler);
+function sheetHandler() {
+    let noofChildren = sheetList.children.length;
+    // dom se create 
+    let newSheet = document.createElement("div");
+    newSheet.setAttribute("class", "sheet");
+    newSheet.setAttribute("sheetIdx", noofChildren);
+    newSheet.textContent = `Sheet ${noofChildren + 1}`
+    sheetList.appendChild(newSheet);
+    initDB();
+    // active me switch
+    newSheet.addEventListener("click", function () {
+        for (let i = 0; i < sheetList.children.length; i++) {
+            sheetList.children[i].classList.remove("active-sheet")
+        }
+        newSheet.classList.add("active-sheet");
+        let idx = newSheet.getAttribute("sheetIdx");
+        db = sheetsDb[idx];
+        setinitUI();
+
+    })
+    newSheet.click();
+}
+
+function sheetOpenHandler() {
+    let noofChildren = sheetList.children.length;
+    // dom se create 
+    let newSheet = document.createElement("div");
+    newSheet.setAttribute("class", "sheet");
+    newSheet.setAttribute("sheetIdx", noofChildren);
+    newSheet.textContent = `Sheet ${noofChildren + 1}`
+    sheetList.appendChild(newSheet);
+    initDB();
+    // active me switch
+    newSheet.addEventListener("click", function () {
+        for (let i = 0; i < sheetList.children.length; i++) {
+            sheetList.children[i].classList.remove("active-sheet")
+        }
+        newSheet.classList.add("active-sheet");
+        let idx = newSheet.getAttribute("sheetIdx");
+        db = sheetsDb[idx];
+        setinitUI();
+    })
+}
+// create sheet logic
+function setinitUI() {
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 26; j++) {
+            //    set all the properties on ui with matching rid,cid
+            let cellObject = db[i][j];
+            let tobeChangedCell = document.querySelector(`.grid .cell[rId='${i}'][cId='${j}']`);
+            tobeChangedCell.innerText = cellObject.value;
+            tobeChangedCell.style.color = cellObject.color;
+            tobeChangedCell.style.backgroundColor = cellObject.backgroundColor;
+            tobeChangedCell.style.fontFamily = cellObject.fontFamily;
+            tobeChangedCell.style.textAlign = cellObject.halign;
+            tobeChangedCell.style.textDecoration = cellObject.underline == false ? "none" : "underline";
+            tobeChangedCell.style.fontStyle = cellObject.italic == false ? "normal" : "italic";
+            tobeChangedCell.style.fontSize = cellObject.fontSize;
+        }
+    }
+}
